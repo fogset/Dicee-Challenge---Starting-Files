da8137061133772e97e852a174672984a0b25294
fogset <zhangth1990@hotmail.com>
2021-01-13 04:25:03 -0800

    change title to display a winner

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ var randomStringImage1 = "images/dice" + randomNumber1 + ".png";
 var randomStringImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img1").setAttribute("src", randomStringImage1);
 document.querySelector(".img2").setAttribute("src", randomStringImage2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
