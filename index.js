4c733ee9f87a0ab08a2905f4cc4b3c871ceea432
fogset <zhangth1990@hotmail.com>
2021-01-13 04:12:16 -0800

    added two random number to random images

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 document.query
+document.query
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomStringImage1 = "images/dice" + randomNumber1 + ".png";
+var randomStringImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img1").setAttribute("src", randomStringImage1);
+document.querySelector(".img2").setAttribute("src", randomStringImage2);
